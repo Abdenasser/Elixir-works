0c63c0d209ac43ab213115ce05bf2832f7d7675f
nasser <nasser.elidrissi065@gmail.com>
2016-02-28 15:44:08 +0000

    first commit

---
+++ ask.ex
@@ -0,0 +1,13 @@
+defmodule Ask do
+  def chars() do
+    IO.puts (
+    """
+    Which planemo are you on ?
+    1. Earth
+    2. Moon
+    3. Mars
+    """
+    )
+    IO.getn("Which? > ") 
+  end
+end
