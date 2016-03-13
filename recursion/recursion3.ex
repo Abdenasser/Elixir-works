6ff47da7f1ac4bc1e82667a9af7bf8800bc576fa
nasser <nasser.elidrissi065@gmail.com>
2016-03-13 19:55:50 +0000

    tail optimized

--- recursion/recursion3.ex
+++ recursion/recursion3.ex
@@ -1,16 +1,13 @@
-## Recursion
-
-# Tail-call optimization
-# If the very last thing a function does is call itself,
-# there is no need to make the call. The runtime can jump
-# (like GOTO) back to the start of the function.
-
-## The recursive call Must be the last function executed
+## Using tail call optimization
+# simply ensure that the last call
+# in the function is the function.
+# Hawever you may need to accumulate the result
 defmodule Recursion3 do
-  def ten_times(0), do: 0
-  def ten_times(n) do
-    # not tail optimized
-    10 + ten_times(n - 1)
+  def ten_times(n), do: ten_times(n, 0)
+  def ten_times(0, acc), do: acc
+  def ten_times(n, acc) do
+    # tail optimized
+    ten_times(n - 1, acc + 10)
   end
 end
-IO.puts Recursion3.ten_times(3)
+IO.puts Recursion3.ten_times(3, 0)
